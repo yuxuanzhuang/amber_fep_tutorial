e5f86d082b66dc7de11cfd4283c535aa9ad6b52d
Yuxuan Zhuang <yuxuan.zhuang@stanford.edu>
2024-11-19 17:47:32 -0800

    use python 3 for analysis

--- ligand/pmemd/free_energy/analyse.sh
+++ ligand/pmemd/free_energy/analyse.sh
@@ -2,7 +2,7 @@
 
 
 top=$(pwd)
-getdvdl=$top/getdvdl.py
+getdvdl=$top/getdvdl_python3.py
 
 for system in ligands complex; do
   cd $system
@@ -11,7 +11,7 @@ for system in ligands complex; do
   for step in decharge vdw_bonded recharge; do
     cd $step
 
-    python $getdvdl 5 ti001.en [01].* > dvdl.dat
+    python3 $getdvdl 5 ti001.en [01].* > dvdl.dat
     echo -n "$system/$step: "
     dG=$(tail -n 1 dvdl.dat | awk '{print $4}')
     echo $dG
